cf7fd10001d83cfa7d76e7094e9942fe4ec579ea
Dean Perry <deanpcmad@gmail.com>
2015-01-26 00:06:38 +0000

    fixed symlink issue and added examples

---
+++ examples/deploy.rb
@@ -0,0 +1,56 @@
+set :application, "website"
+set :deploy_user, "deploy"
+
+# setup repo details
+set :scm, :git
+set :repo_url, ""
+
+# setup rbenv.
+set :rbenv_type, :system
+set :rbenv_ruby, "2.1.5"
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+
+# how many old releases do we want to keep
+set :keep_releases, 5
+
+# files we want symlinking to specific entries in shared.
+set :linked_files, %w{config/database.yml}
+
+# dirs we want symlinking to shared
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# what specs should be run before deployment is allowed to
+# continue, see lib/capistrano/tasks/run_tests.cap
+set :tests, []
+
+# this:
+# http://www.capistranorb.com/documentation/getting-started/flow/
+# is worth reading for a quick overview of what tasks are called
+# and when for `cap stage deploy`
+
+namespace :deploy do
+  # make sure we're deploying what we think we're deploying
+  before :deploy, "deploy:check_revision"
+
+  # only allow a deploy with passing tests to be deployed
+  # before :deploy, "deploy:run_tests"
+
+  after :finishing, 'deploy:cleanup'
+
+  # remove the default nginx configuration as it will tend
+  # to conflict with our configs.
+  before 'deploy:setup_config', 'nginx:remove_default_vhost'
+
+  # reload nginx to it will pick up any modified vhosts from
+  # setup_config
+  after 'deploy:setup_config', 'nginx:reload'
+
+  # Restart monit so it will pick up any monit configurations
+  # we've added
+  after 'deploy:setup_config', 'monit:restart'
+
+  # As of Capistrano 3.1, the `deploy:restart` task is not called
+  # automatically.
+  after 'deploy:publishing', 'deploy:restart'
+end
